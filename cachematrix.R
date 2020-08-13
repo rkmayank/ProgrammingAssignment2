21accb1e047e9ae50b0f2a44cf6db7fdcdf0c634
rkmayank <65361752+rkmayank@users.noreply.github.com>
2020-08-13 21:47:56 +0530

    Programming Assingnment Inverse of a Matrix by Cache Memory
    makeCacheMatrix <- function (x = matrix()){
    inv <- NULL
    Set <- function(y){
    x <<- y
    inv<< - NULL
    }
    }
    get <- function () {x}
    SetInverse <- function( Inverse){inv<<- inverse}
    getInverse <- function() {inv}
    list ( set = set, get = get, SetInverse = SetInverse,getInverse = getInverse)
    }
    Cachesolve <- function(x,...){
    inv<- x$getInverse()
    if(!is.null(inv)){
    message("getting cache data")
    return(inv)
    }
    mat <- x$get()
    inv <- solve(mat, ...)
    x$SetInverse(inv)
    inv
    }

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+---
+title: "My First R Markdown filw"
+author: "Apurva Mayank"
+date: "5/24/2020"
+output: pdf_document
+---
 
-## Write a short comment describing this function
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
 
-makeCacheMatrix <- function(x = matrix()) {
+## R Markdown
 
-}
+This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
 
+When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
 
-## Write a short comment describing this function
+```{r cars}
+summary(cars)
+```
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Including Plots
+
+You can also embed plots, for example:
+
+```{r pressure, echo=FALSE}
+plot(pressure)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
